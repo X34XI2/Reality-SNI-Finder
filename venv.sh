764f5e090b5f6df765f6bc3d246fd5b033564007
X34XI2 <74297352+X34XI2@users.noreply.github.com>
2025-05-13 22:46:20 +0400

    Update venv.sh

--- venv.sh
+++ venv.sh
@@ -1,5 +1,7 @@
+sudo apt install -y python3-dev python3-pip python3-setuptools build-essential libatlas-base-dev gfortran
 python3 -m venv sni             
 source sni/bin/activate
 cd /root/Reality-SNI-finder
+pip install --upgrade pip setuptools wheel
 pip install -r requirements.txt 
  
