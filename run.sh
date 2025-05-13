604d0c5c70d1eb4fd730616fed06ecd9886db735
X34XI2 <74297352+X34XI2@users.noreply.github.com>
2025-05-13 21:40:44 +0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ main() {
     sudo apt install git pip -y
     git clone https://github.com/meower1/Reality-SNI-finder.git
     (cd /root/Reality-SNI-finder)
-    (sudo apt install python3-venv -y && python3 -m venv SNI && source venv/bin/activate)
+    (sudo apt install python3-venv -y && python3 -m venv SNI && source SNI/bin/activate && pip install panda && pip install tabulate && pip install re && pip install subprocess)
     (cd /root/Reality-SNI-finder && python3 main.py)
 }
 
