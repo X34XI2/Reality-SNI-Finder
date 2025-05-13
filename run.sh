e5cbd615b89a0c231c9e9d897ae75fb1556b158d
X34XI2 <74297352+X34XI2@users.noreply.github.com>
2025-05-13 21:19:43 +0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,8 +4,8 @@ main() {
     sudo apt update
     sudo apt install git pip -y
     git clone https://github.com/meower1/Reality-SNI-finder.git
-    (cd Reality-SNI-finder && pip install -r requirements.txt) 
-    (cd Reality-SNI-finder && python3 main.py)
+    (cd /root/Reality-SNI-finder && pip install -r requirements.txt) 
+    (cd /root/Reality-SNI-finder && python3 main.py)
 }
 
 main
