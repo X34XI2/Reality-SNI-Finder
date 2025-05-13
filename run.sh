4229a90bdb8270b92cb2368d8f22d4eec311de96
X34XI2 <74297352+X34XI2@users.noreply.github.com>
2025-05-13 22:14:11 +0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ main() {
     sudo apt update
     sudo apt install git pip -y
     git clone https://github.com/X34XI2/Reality-SNI-finder.git
-    (cd /root/Reality-SNI-finder && sudo apt install python3-venv -y && chmod +x /root/Reality-SNI-finder/venv.sh)
+    (cd /root/Reality-SNI-finder && pip install -r requirements.txt)
     (cd /root/Reality-SNI-finder && python3 main.py)
 }
 main
